e29b063aaeb5009e5f6490e00b861edf0ec79019
Alex Galonsky <alex@galonsky.com>
2021-02-02 21:27:44 -0500

    removing celery and redis

--- ddw/twitter.py
+++ ddw/twitter.py
@@ -3,9 +3,7 @@ from datetime import datetime, timezone, timedelta
 
 import twitter
 
-from ddw.caching import cache
 from ddw.models import Tweet
-from ddw.redis import require_lock
 
 api = twitter.Api(
     consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),
@@ -18,14 +16,12 @@ SCREEN_NAME = "diddukewin"
 MIN_HOURS_BETWEEN_TWEETS = int(os.getenv("MIN_HOURS_BETWEEN_TWEETS", 8))
 
 
-@cache.cache_result("latest_tweet", 60 * 60)
 def get_latest_tweet() -> dict:
     statuses = api.GetUserTimeline(screen_name=SCREEN_NAME, count=1)
     status = statuses[0]
     return {"text": status.text, "created_at": status.created_at}
 
 
-@require_lock("tweeting-lock", 30)
 def post_tweet(tweet_text: str):
     latest_tweet_dict = get_latest_tweet()
     latest_tweet = Tweet.from_tweet_dict(latest_tweet_dict)
@@ -41,5 +37,4 @@ def post_tweet(tweet_text: str):
     if latest_tweet.text_without_link == new_tweet.text_without_link:
         return
 
-    cache.invalidate("latest_tweet")
     api.PostUpdate(tweet_text)
--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -3,7 +3,6 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from ddw.caching import cache
 from ddw.models import Tweet
 from ddw.twitter import get_latest_tweet, post_tweet
 
@@ -14,10 +13,6 @@ def mock_api(mocker):
 
 
 class TestGetLatestTweet:
-    @pytest.fixture(autouse=True)
-    def mock_cache(self, mocker):
-        mocker.patch.object(cache, "get_cache", return_value=None)
-        mocker.patch.object(cache, "set_cache")
 
     def test_grabs_first(self, mock_api):
         status = MagicMock()
@@ -30,9 +25,6 @@ class TestGetLatestTweet:
 
 
 class TestPostTweet:
-    @pytest.fixture(autouse=True)
-    def r_mock(self, mocker):
-        return mocker.patch("ddw.redis.r.lock")
 
     @pytest.fixture
     def mock_get_latest_tweet(self, mocker):
@@ -41,10 +33,6 @@ class TestPostTweet:
     @pytest.fixture
     def mock_from_tweet_dict(self, mocker):
         return mocker.patch.object(Tweet, "from_tweet_dict")
-
-    @pytest.fixture
-    def mock_cache(self, mocker):
-        return mocker.patch("ddw.twitter.cache")
 
     def test_too_early_doesnt_post(
         self, mock_get_latest_tweet, mock_from_tweet_dict, mock_api
@@ -67,12 +55,11 @@ class TestPostTweet:
         mock_api.PostUpdate.assert_not_called()
 
     def test_tweets(
-        self, mock_get_latest_tweet, mock_from_tweet_dict, mock_api, mock_cache
+        self, mock_get_latest_tweet, mock_from_tweet_dict, mock_api,
     ):
         mock_from_tweet_dict.return_value = Tweet(
             "bar",
             datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=9),
         )
         post_tweet("foo")
-        mock_cache.invalidate.assert_called_once_with("latest_tweet")
         mock_api.PostUpdate.assert_called_once_with("foo")
